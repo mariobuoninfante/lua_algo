6aa9cb742db969993c4ec24671c3a78fae45226b
mariobuoninfante <mario.buoninfante@gmail.com>
2020-04-23 00:39:46 +0100

    added sorting folder with selection and insertion algorithms

---
+++ sorting_algo/Sort.lua
@@ -0,0 +1,54 @@
+-- sort algorithms
+
+local Sort = {}
+
+function Sort.selection(T, direction)
+    -- args: T = table to sort; direction = 0/1 ascending/descending  
+    
+    -- check 'direction' variable and create a 'compare' function
+    direction = direction or 0  
+    local compare = function (a, b) return a < b end
+    if direction == 1 then
+        compare = function (a, b) return a > b end
+    end
+    -- get table size
+    local N = #T 
+    local min_max = 1
+    -- re-order the table
+    for i = 1, N do 
+        min_max = i
+        for j = i+1, N do 
+            -- find the min/max
+            if compare(T[j], T[min_max]) then 
+                min_max = j
+            end
+        end
+        T[i], T[min_max] = T[min_max], T[i]
+    end
+end
+
+function Sort.insertion(T, direction)
+    -- args: T = table to sort; direction = 0/1 ascending/descending  
+
+    -- check 'direction' variable and create a 'compare' function
+    direction = direction or 0  
+    local compare = function (a, b) return a < b end
+    if direction == 1 then
+        compare = function (a, b) return a > b end
+    end    
+    -- get the length of the table
+    local N = #T
+    local j = 1
+    for i = 2, N do
+        j = i 
+        -- sort all elements from ith to 1st
+        while (j > 1 and compare(T[j],T[j-1])) 
+        do 
+            T[j], T[j-1] = T[j-1], T[j]
+            j = j - 1
+        end
+    end
+end
+
+
+return Sort
---
+++ sorting_algo/main.lua
@@ -0,0 +1,28 @@
+local sort = require("Sort")
+
+math.randomseed(os.time())  -- 'random' seed
+
+local function rand_table_gen()
+    -- random table generator
+    outcome = {}
+    local _start = 5
+    local _length = math.random(_start, 25)
+    local _end = _start + _length
+    for i = 1, _length do 
+        outcome[i] = math.random(100)
+    end
+    return outcome
+end
+
+local function print_table(T)
+    for _, i in ipairs(T) do 
+        io.write(i, " ")
+    end
+    io.write("\n")
+end
+
+local r_table = rand_table_gen()
+print_table(r_table)
+-- sort.selection(r_table, 1)  -- 2nd arg: 0/1 ascending/descending order
+sort.insertion(r_table, 1)  -- 2nd arg: 0/1 ascending/descending order
+print_table(r_table)
